b1c6b2181ba9fcb98bf0588f0b7dcc76d56f5cbd
bruno00009 <161394551+bruno00009@users.noreply.github.com>
2024-11-27 22:11:08 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação',
+    'quem sou eu?',
+    'adriele '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz meu nome em ingles?',
+    ' assim se diz em ingles my name is'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Cor favorita?',
+    ' azul'
+)
